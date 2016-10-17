fb7d18fe27c28b29bc2549e2b23fd4a104b746a8
Joshua Berry <joshua.berry92@gmail.com>
2016-10-17 09:41:11 +0100

    Setting up tings

---
+++ js/components/layout.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Layout extends Component {
+  render () {
+    return(
+      <nav>
+
+        <h1>Lunchify</h1>
+
+      </nav>
+    );
+  }
+}
+
+
+export default Layout;
---
+++ js/index.jsx
@@ -0,0 +1,6 @@
+import Greeting from "./components/layout.jsx"
+import React, { Component } from 'react'
+
+ReactDOM.render(
+  <Greeting />, document.getElementById('container')
+)
